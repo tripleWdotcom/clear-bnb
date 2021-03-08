48afadcd71059539244b946df7a846aa8815b7b1
tripleWdotcom <mauricio.espinoza@gmail.com>
2021-03-08 12:52:06 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Yhellow everyone!!!
         </a>
       </header>
     </div>
