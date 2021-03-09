356ab0515e0c7e1b3559caec9473adccf78865b3
tripleWdotcom <mauricio.espinoza@gmail.com>
2021-03-09 23:04:36 +0100

    Revert "Revert "Revert "populate works"""
    This reverts commit 80d038306cdf134a0d712094c5eeab8f00bb7aeb.

--- proxy-settings.js
+++ proxy-settings.js
@@ -2,7 +2,7 @@ module.exports = {
   // run react-dev-server (true) or serve build (false)
   dev: true,
   // whether to open the react site in a browser at start
-  openInBrowser: false,
+  openInBrowser: true,
   // path to your own backend api
   // (set to empty string if yo don't have an internal JS-based api)
   pathToAPI: './api/index',
