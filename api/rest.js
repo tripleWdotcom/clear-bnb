2de30b7ca99d126f665a8860c08d7f78b651b35c
bextex <r.sarvegard@gmail.com>
2021-03-10 12:22:30 +0100

    added filter with city as a query
    Co-Authored-By: tripleWdotcom <67017215+tripleWdotcom@users.noreply.github.com>

--- api/rest.js
+++ api/rest.js
@@ -8,30 +8,60 @@ module.exports = (app, models) => {
     res.json(docs)
   })
 
-  // Get houses by filters 
-  app.get('/rest/:model/filters/:something', async (req, res) => {
-    console.log(req.params.something)
-    let filter = JSON.parse(req.params.something)
-    console.log(JSON.parse(req.params.something))
-    console.log(filter[0])
+  app.get('/rest/:model/city/:city/', async (req, res) => {
+    let city = req.params.city
     let model = models[req.params.model]
-    console.log(model)
-    let docs = await model.find({ $and: [filter[0], filter[1]]})
+    let docs = await model.find({ city: { $regex: '^' + city, $options: 'i' }})
     console.log(docs)
-    //console.log(req.body)
-    // let filterObj = JSON.parse(req.body)
-    // console.log(filterObj)
-  //   let docs = await houses.find({ featuresId: filterObj })
     res.json(docs)
   })
+
+  // Get houses by filters 
+  app.get('/rest/:model/filters/:city/', async (req, res) => {
+    let bedrooms = parseInt(req.params.bedrooms)
+    let country = req.params.country
+    let pool = parseInt(req.params.pool)
+
+    console.log(bedrooms)
+    console.log(country)
+    console.log(pool)
+  //   let filter = JSON.parse(req.params.something)
+  //   console.log(JSON.parse(req.params.something))
+  //   console.log(filter[0])
+    let model = models[req.params.model]
+  //   console.log(model)
+    let docs = await model.find({featureIds: '6046bf371807457c80418887'}).populate('featureIds').count()
+    //let docs = await model.find({ country: country })
+    //let docs = await model.find({ $and: [{ bedrooms: { $lt: bedrooms }, country: country}]})
+  //   console.log(docs)
+  //   //console.log(req.body)
+  //   // let filterObj = JSON.parse(req.body)
+  //   // console.log(filterObj)
+  // //   let docs = await houses.find({ featuresId: filterObj })
+    console.log(docs)
+    res.json(docs)
+  })
+
+  // 3001 / rest / houses / filters / [{ "bedrooms": 4 }]
+  // 3001 / rest / houses / filters / bedrooms / :bedrooms / wifi / :wifi / parking /:parking
+  //   let bedrooms = req.params.bedrooms
+  //   let wifi = req.params.wifi
+  //   let parking = req.params.parking
+  
+  
+  // 3001 / rest / houses / filters / { "bedrooms": 4, "wifi": true }
+  // filter.bedrooms
+  // filter.wifi
+  // filter.parking
+  
+
+
 
   // Get user/house/rental by id
   app.get('/rest/:model/:id', async (req, res) => {
     let model = models[req.params.model]
-    // Only populate if ref exists?   answer: yes
-   // let docs = await model.findById(req.params.id).populate(['userId', 'houseId', 'featureIds']).exec()
-    let docs = await model.findById(req.params.id).populate(['userId','featureIds']).exec()
-
+    // Only populate if ref exists?
+    let docs = await model.findById(req.params.id).populate(['userId', 'houseId', 'featureIds']).exec()
     res.json(docs)
   })
 
