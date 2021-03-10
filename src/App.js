2ecfe3a26918130970eb6573bf9e4e3572e07224
bextex <r.sarvegard@gmail.com>
2021-03-10 22:00:50 +0100

    added comment
    Co-Authored-By: tripleWdotcom <67017215+tripleWdotcom@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hello World, i mean React!
         </a>
       </header>
     </div>
