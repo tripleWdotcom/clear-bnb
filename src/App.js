3f610127e84d1e6fe20e17d78cb9a5b02bc2c2f5
yunyan2020 <li_yunyan@hotmail.com>
2021-03-12 11:40:19 +0100

    burger-function haven't been finished.
    Delete temp test Burger files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
-<<<<<<< Updated upstream
-import Menu from './components/Menu.js'
-import Navbar from './components/Navbar';
-import Home from './pages/Home';
-=======
 import MemberPage from './components/MemberPage.js'
 import Home from './pages/Home';
 import MyPage from './pages/MyPages.js'
-// import Burger from './components/Burger.js'
-
->>>>>>> Stashed changes
 
 function App() {
 
 
 
-<<<<<<< Updated upstream
   function App() {
 
     const page404 = () => (
@@ -26,21 +17,7 @@ function App() {
 
     return (
       <div className="App">
-
-        <Router>
-          <header className={true ? 'App-Header' : ''}>
-            <Navbar />
-            <Menu />
-          </header>
-
-
-          <main>
-            <Switch>
-              <Route exact path="/" component={Home} />
-              <Route path="*" component={page404} />
-            </Switch>
-          </main>
-=======
+       
       <Router>
         <header className={true ? 'App-Header' : ''}>
           <MemberPage />
@@ -54,7 +31,6 @@ function App() {
             <Route path="*" component={page404} />
           </Switch>
         </main>
->>>>>>> Stashed changes
 
         </Router>
 
