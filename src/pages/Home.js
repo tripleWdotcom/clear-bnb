4f9b232093c359de8e4b1061c0106b224897d77b
tripleWdotcom <mauricio.espinoza@gmail.com>
2021-03-17 23:26:02 +0100

    Testing dateRange picker
    Calendar works and out put values correct values

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,11 +5,28 @@ import { makeStyles } from '@material-ui/core/styles';
 import Hidden from '@material-ui/core/Hidden';
 import SearchComponent from '../components/Search'
 import CityIconList from '../components/CityIconList'
+import { RangePicker } from '../components/Calendar';
+import 'react-date-range/dist/styles.css'; // main css file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+import { DateRange } from 'react-date-range';
+import { useState } from 'react'
 
 
 
 const Home = () => {
   const classes = useStyles();
+  const [state, setState] = useState([
+    {
+      startDate: new Date(),
+      endDate: null,
+      key: 'selection'
+    }
+  ]);
+
+  let sd=state[0].startDate
+  let ed = state[0].endDate
+  console.log("start date: ", state[0].startDate," In timestamp:", sd.getTime())
+  console.log("end date:", state[0].endDate," In timestamp:", (ed?ed.getTime():"Not defined yet"))
 
   return (
     <div className="home">
@@ -19,6 +36,16 @@ const Home = () => {
           <Grid item xs={12}>
             <Paper className={classes.paper}>Search component
             <SearchComponent />
+              <DateRange
+                editableDateInputs={true}
+                onChange={item => setState([item.selection])}
+                moveRangeOnFirstSelection={false}
+                ranges={state}
+              >
+         
+           
+
+              </DateRange>
             </Paper>
           </Grid>
 
