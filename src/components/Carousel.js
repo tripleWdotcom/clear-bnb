d0fbc8357f96ce302100f44fdde3aba23283b887
Mikael <mikael.sardor@studerande.plushogskolan.se>
2021-03-18 22:52:22 +0100

    Imported Radium and Grid in Carousel.js

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,14 +1,17 @@
-
+import Radium from 'radium'
+import Grid from '@material-ui/core/Grid';
 import { Carousel } from "react-responsive-carousel";
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
 const Ok= () => {
   return(
+      <Grid style={styles.pic1}>
     <Carousel autoPlay showArrows={false} showThumbs={false} showIndicators={false} infiniteLoop={true} showStatus={false}>
     <div>
         <img alt="" src="https://image.shutterstock.com/shutterstock/photos/1702125637/display_1500/stock-photo-new-construction-homes-in-texas-built-in-1702125637.jpg" />
 
-    </div>
+        </div>
+      
     <div>
         <img alt="" src="https://image.shutterstock.com/shutterstock/photos/683283169/display_1500/stock-photo-big-modern-beautiful-house-683283169.jpg" />
 
@@ -20,10 +23,18 @@ const Ok= () => {
     <div>
         <img alt="" src="https://image.shutterstock.com/shutterstock/photos/1702012048/display_1500/stock-photo-real-estate-exterior-front-house-1702012048.jpg" />
 
-    </div>
-
+      </div>
+    
   </Carousel>
+      </Grid>
   
 );
-  }
-export default Ok
+}
+
+const styles = {
+  pic1: {
+    width: '900px'
+  },
+}
+
+export default Radium(Ok)
