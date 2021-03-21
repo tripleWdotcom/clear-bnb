a1810f6bcae4cde51c427b314c812ab89c3c3580
bextex <r.sarvegard@gmail.com>
2021-03-21 11:49:17 +0100

    added range for price per night

--- src/components/MyPages/AddNewRental.js
+++ src/components/MyPages/AddNewRental.js
@@ -46,51 +46,13 @@ function AddNewRental() {
 
   }
 
-  const marks = [
-    {
-      value: 0,
-      label: '1',
-    },
-    {
-      value: 1,
-      label: '2',
-    },
-    {
-      value: 2,
-      label: '3',
-    },
-    {
-      value: 3,
-      label: '4',
-    },
-    {
-      value: 4,
-      label: '5',
-    },
-    {
-      value: 5,
-      label: '6',
-    },
-    {
-      value: 6,
-      label: '7',
-    },
-    {
-      value: 7,
-      label: '8',
-    },
-    {
-      value: 8,
-      label: '9',
-    },
-    {
-      value: 9,
-      label: '10',
-    },
-  ];
 
 
-  function valuetext(value) {
+
+  function valuetextBeds(value) {
+    return `${value}`;
+  }
+  function valuetextPrice(value) {
     return `${value}`;
   }
 
@@ -167,13 +129,33 @@ function AddNewRental() {
             </input>
           </label>
           <button>Add more pics</button>
+          <br />
+          <br />
+          <br />
+          <label>
+            <Slider
+              defaultValue={1}
+              getAriaValueText={valuetextBeds}
+              step={1}
+              valueLabelDisplay="on"
+              max={10}
+              min={1}
+              onChange={(e) => setBeds(e.target.value)}
+            />
+            Number of beds
+          </label>
+          <label>
           <Slider
             defaultValue={1}
-            getAriaValueText={valuetext}
-            step={1}
-            marks={marks}
+            getAriaValueText={valuetextPrice}
+            step={10}
             valueLabelDisplay="on"
+            max={1000}
+            min={10}
+            onChange={(e) => setPrice(e.target.value)}
           />
+            Price per night $
+          </label>
           <button style={{ ...modalStyle.button, ...modalStyle.btnIn }} key="11">Create rental</button>
         </form>
       </div>
