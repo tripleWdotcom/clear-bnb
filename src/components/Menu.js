d16ba65a000d16562357c272e07a26b4d807c16c
bextex <r.sarvegard@gmail.com>
2021-03-30 14:53:26 +0200

    changed som style for mypage

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -25,10 +25,10 @@ function MenuContainer(props) {
       <Hidden xsDown>
         <Grid container spacing={3} direction="column" style={style.containerDesktop} key="5">
           <Grid item xs>
-            <h4 style={style.titleDesktop} key="6" onClick={() => { sendNewAction('showBookings') }}>My bookings</h4>
+            <h3 style={style.titleDesktop} key="6" onClick={() => { sendNewAction('showBookings') }}>My bookings</h3>
           </Grid>
-          <Grid item xs><h4 style={style.titleDesktop} key="7" onClick={() => { sendNewAction('showRentals') }}>My rentals</h4></Grid>
-          <Grid item xs><h4 style={style.titleDesktop} key="8" onClick={() => { sendNewAction('showNewRental') }}>Add new rental</h4></Grid>
+          <Grid item xs><h3 style={style.titleDesktop} key="7" onClick={() => { sendNewAction('showRentals') }}>My rentals</h3></Grid>
+          <Grid item xs><h3 style={style.titleDesktop} key="8" onClick={() => { sendNewAction('showNewRental') }}>Add new rental</h3></Grid>
         </Grid>
       </Hidden>
 
@@ -63,7 +63,8 @@ const style = {
     marginTop: '45px',
     padding: '20px',
     borderRadius: '20px',
-    backgroundColor: 'white'
+    backgroundColor: 'white',
+    opacity: '0.9'
   },
   titleDesktop: {
     transition: 'all 500ms ease-in-out',
